40d1801af1cf084b9cdccf46b6ff7a505c0ee15e
Aakash Bapna <aakash.bapna@gmail.com>
2022-08-03 22:23:00 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { NotionPageToHtml } from './main/use-cases/notion-api-to-html';
 import { NotionApiContentResponsesToBlocks } from "./infra/use-cases/to-blocks/notion-api-content-response-to-blocks';
-
+import {makeBlocksToHtml} from "./main/factories"
 
 export NotionApiContentResponsesToBlocks;
+export makeBlocksToHtml;
